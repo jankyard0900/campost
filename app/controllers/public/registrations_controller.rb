0838e3a2a947e282c70e99ae2da1f867ecd21195
jankyard0900 <112614647+jankyard0900@users.noreply.github.com>
2022-11-10 09:34:11 +0000

    [add]会員登録、ログイン

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -3,7 +3,8 @@
 class Public::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
   # GET /resource/sign_up
   # def new
   #   super
@@ -59,4 +60,14 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+  protected
+  
+  def after_sign_up_path_for(resource)
+    my_page_path
+  end
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
