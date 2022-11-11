0f5bd2656f28d8d07da778df3f1f70b8793b81e6
jankyard0900 <112614647+jankyard0900@users.noreply.github.com>
2022-11-11 00:38:16 +0000

    [add]管理者会員一覽

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,13 +1,28 @@
 class Admin::CustomersController < ApplicationController
   def index
+    @customers = Customer.page(params[:page])
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path(@customer.id), notice: "会員情報を編集しました。"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:name, :email, :is_active)
   end
 end
--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -4,7 +4,7 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
   # GET /resource/sign_up
   # def new
   #   super
@@ -62,11 +62,11 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # end
 
   protected
-  
+
   def after_sign_up_path_for(resource)
-    my_page_path
+    customer_path(current_customer)
   end
-  
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
