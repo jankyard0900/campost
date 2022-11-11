a6352be5079cd5017e3e6cdfbc7a01de3f1a99b5
jankyard0900 <112614647+jankyard0900@users.noreply.github.com>
2022-11-11 02:29:36 +0000

    [rest]キャンプ場投稿画面

--- app/controllers/public/camps_controller.rb
+++ app/controllers/public/camps_controller.rb
@@ -1,25 +1,48 @@
 class Public::CampsController < ApplicationController
   def new
+    @camp = Camp.new
   end
 
   def create
+    @camp = Camp.new(camp_params)
+    if @camp.save
+      flash[:success] = "キャンプ場が作成できました！"
+      redirect_to camps_path
+    else
+      render :new
+    end
   end
 
   def index
+    @camps = Camp.page(params[:page]).per(6)
   end
 
   def show
   end
 
   def edit
+    @camp = Camp.find(params[:id])
   end
 
   def update
   end
 
   def destroy
+    @camp = Camp.find(params[:id])
+    if @camp.destroy
+      flash[:success] = "投稿を削除しました。"
+      redirect_to camps_path
+    else
+      render :edit
+    end
   end
 
   def search
+  end
+
+  private
+
+  def camp_params
+    params.require(:camp).permit(:name, :area_id, :name, :address, :access_method, :parking, :vehicle, :close_facilities, :hot_spring,  :in_site_facilities, :fee_information, camp_images: [])
   end
 end
