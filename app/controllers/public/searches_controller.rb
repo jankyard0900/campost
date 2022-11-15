5ddb28404c37c2ff6db11e774acbeb9efc4386e9
jankyard0900 <112614647+jankyard0900@users.noreply.github.com>
2022-11-15 09:55:24 +0000

    [add]検索機能追加

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,10 +1,13 @@
 class Public::SearchesController < ApplicationController
   def search
+    @areas = Area.all
+    @categories = Category.all
+    @word = params[:word]
     @range = params[:range]
     if @range == "キャンプ場"
-      @camps = Camp.looks(params[:word])
+      @camps = Camp.looks(params[:word]).page(params[:page]).per(6)
     else
-      @gears = Gear.looks(params[:word])
+      @gears = Gear.looks(params[:word]).page(params[:page]).per(6)
     end
   end
 end
