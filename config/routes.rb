cc580fd1553c7236a86a1e2147eeb54f61010930
jankyard0900 <112614647+jankyard0900@users.noreply.github.com>
2022-11-16 03:38:05 +0000

    [add]ゲストログイン途中・マイページギア星表示

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
     sessions: 'public/sessions'
   }
 
+  devise_scope :customer do
+  post 'cusotmers/guest_sign_in'=>'public/sessions#guest_sign_in'
+  end
+
   # 管理者用
   # URL /admin/sign_in ...
   devise_for :admin,skip: [:registrations, :passwords], controllers: {
@@ -43,6 +47,7 @@ Rails.application.routes.draw do
 
     resources :areas, only: [:create, :edit, :update]
   end
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
