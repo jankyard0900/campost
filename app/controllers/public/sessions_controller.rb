ce33e4641d3c4ddf5c1f40f536143577af719cd5
jankyard0900 <112614647+jankyard0900@users.noreply.github.com>
2022-11-21 06:13:14 +0000

    notice→alertに変更

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -39,7 +39,7 @@ class Public::SessionsController < Devise::SessionsController
     @customer = Customer.find_by(email: params[:customer][:email])
     if @customer
       if @customer.valid_password?(params[:customer][:password]) && (@customer.is_active == false)
-        flash[:notice] = "退会済みです。再度ご登録をしてご利用ください"
+        flash[:alert] = "退会済みです。再度ご登録をしてご利用ください"
         redirect_to new_customer_registration_path
       else
         flash[:notice] = "項目を入力してください。"
