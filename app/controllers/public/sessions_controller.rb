4ffbac15e26556826d5dc4d9844888033fc872bd
jankyard0900 <112614647+jankyard0900@users.noreply.github.com>
2022-11-21 07:14:01 +0000

    会員詳細レイアウト変更

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -41,8 +41,6 @@ class Public::SessionsController < Devise::SessionsController
       if @customer.valid_password?(params[:customer][:password]) && (@customer.is_active == false)
         flash[:alert] = "退会済みです。再度ご登録をしてご利用ください"
         redirect_to new_customer_registration_path
-      else
-        flash[:notice] = "項目を入力してください。"
       end
     end
   end
